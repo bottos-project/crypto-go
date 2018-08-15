79edee5305fecaf89d020ee7d56cbfed7287166e
zhangwei <zhangwei@bottos.org>
2018-08-15 14:57:07 +0800

    add test

--- crypto/signature_test.go
+++ crypto/signature_test.go
@@ -1,9 +1,16 @@
 package crypto
 
 import "testing"
+import "fmt"
+import "encoding/hex"
 
 func Test_GenerateKey(t *testing.T) {
-	t.Log(GenerateKey())
+	//t.Log(GenerateKey())
+	x,y := GenerateKey()
+	fmt.Println(hex.EncodeToString(x))
+    fmt.Println(hex.EncodeToString(y))
+	
 }
+
 
 
